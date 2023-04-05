75397f7309626e02da34f9334d244b4663869edc
bapt2 <5711@holbertonstudents.com>
2023-04-05 09:26:47 +0200

    adding a condition

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,14 +8,16 @@ returns information about his/her TODO list progress.
 import requests
 
 
-api_url = requests.get('https://jsonplaceholder.typicode.com/todos/1')
 
-EMPLOYEE_NAME = api_url.json()[0][""]
-NUMBER_OF_DONE_TASKS = api_url.json()[0][""]
-TOTAL_NUMBER_OF_TASKS = api_url.json()[0][""]
-TASK_TITLE = api_url.json()[0][""]
+if __name__ == "__main__":
+    api_url = requests.get('https://jsonplaceholder.typicode.com/todos/1')
 
-print("Employee {} is done with tasks({}/{})".format(EMPLOYEE_NAME,
-                                                     NUMBER_OF_DONE_TASKS,
-                                                     TOTAL_NUMBER_OF_TASKS))
-print("     {}".format(TASK_TITLE))
+    EMPLOYEE_NAME = api_url.json()[0][""]
+    NUMBER_OF_DONE_TASKS = api_url.json()[0][""]
+    TOTAL_NUMBER_OF_TASKS = api_url.json()[0][""]
+    TASK_TITLE = api_url.json()[0][""]
+
+    print("Employee {} is done with tasks({}/{})".format(EMPLOYEE_NAME,
+                                                         NUMBER_OF_DONE_TASKS,
+                                                         TOTAL_NUMBER_OF_TASKS))
+    print("     {}".format(TASK_TITLE))
