b270247574efa9bbfdf8fd77c83c89e4fc7a939d
bapt2 <5711@holbertonstudents.com>
2023-04-06 10:13:00 +0200

    refactoring my code

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -15,33 +15,11 @@ if __name__ == "__main__":
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     data_user = user.json()
     data_todos = todos.json()
-    data_name = ""
     data_username = ""
-    Title = ""
-    task_status = ""
-    count_true = 0
-    total_count = 0
-    userId = 0
 
     for name in data_user:
         if int(argv[1]) == name.get("id"):
-            data_name = name.get("name")
             data_username = name.get("username")
-
-    for data in data_todos:
-        if int(argv[1]) == data.get("userId"):
-            userId = data.get("userId")
-            if data.get("completed") is True:
-                count_true += 1
-
-            if data.get("completed") is True or data.get("completed") is False:
-                task_status = data.get("completed")
-                total_count += 1
-
-    for data in data_todos:
-        if int(argv[1]) == data.get("userId"):
-            if data.get("completed") is True:
-                Title = data.get("title")
 
     with open('USER_ID.csv', 'w') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL, delimiter=',')
